ff5f15a55ab1e653c5a2988b2a670dffe402092b
nickygencs17 <nick_genco@hotmail.com>
2018-01-20 19:08:00 -0500

    new commit

--- three_gifts.py
+++ three_gifts.py
@@ -13,6 +13,7 @@ with open(sys.argv[1], 'r') as f:
 potential_sum = 0
 x_index = 0
 y_index = 0
+z_index = 0
 for x in range(0, len(_list)):
   number = int(_list[x][1])
   for y in range(x, len(_list)):
@@ -25,6 +26,7 @@ for x in range(0, len(_list)):
                 print _list[x][0] + _list[x][1]+", "+ _list[y][0] + _list[y][1] + ", "+ _list[z][0] + _list[z][1]
                 sys.exit()
         if(temp_sum<target_sum):
+          if(x!=y and x!=z and y!=z):
             if(temp_sum>potential_sum):
                 potential_sum = temp_sum
                 x_index = x
